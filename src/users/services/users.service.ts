2ce450aee57a967f5e6fe7eaadf9588f36547a70
vegadelalyra <lyradeperseo@gmail.com>
2023-09-10 22:52:11 -0500

    feat: add guard clauses to the user register

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common'
+import {
+    ConflictException,
+    Injectable,
+    NotFoundException,
+} from '@nestjs/common'
 import { User } from '../entities/user.entity'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
@@ -10,13 +14,23 @@ import { Role } from 'src/roles/entities/role.entity'
 export class UsersService {
     constructor(
         @InjectRepository(User) private readonly userRepository: Repository<User>,
-        @InjectRepository(Role) private readonly roleRepository: Repository<Role>
+        @InjectRepository(Role) private readonly roleRepository: Repository<Role>,
     ) { }
 
     async createUser(createUserDto: CreateUserDto): Promise<User> {
-        const userRole = await this.roleRepository.save({ name: 'user' });
-        const user = this.userRepository.create({ ...createUserDto, roleId: userRole.id });
-        return this.userRepository.save(user);
+        const existingUser = await this.findUserByEmail(createUserDto.email)
+
+        if (existingUser)
+            throw new ConflictException(
+                `User with email ${createUserDto.email} is already registered.`,
+            )
+
+        const userRole = await this.roleRepository.save({ name: 'user' })
+        const user = this.userRepository.create({
+            ...createUserDto,
+            roleId: userRole.id,
+        })
+        return this.userRepository.save(user)
     }
 
     async findAllUsers(): Promise<User[]> {
@@ -31,6 +45,10 @@ export class UsersService {
         return user
     }
 
+    async findUserByEmail(email: string): Promise<User | undefined> {
+        return this.userRepository.findOne({ where: { email } })
+    }
+
     async updateUser(id: number, updateUserDto: UpdateUserDto): Promise<User> {
         const user = await this.findUserById(id)
         this.userRepository.merge(user, updateUserDto)
@@ -39,6 +57,11 @@ export class UsersService {
 
     async deleteUser(id: number): Promise<void> {
         const user = await this.findUserById(id)
-        await this.userRepository.remove(user)
+
+        if (user.is_deleted)
+            throw new ConflictException(`User with ID ${id} is already deleted.`)
+
+        user.is_deleted = true
+        await this.userRepository.save(user)
     }
 }
